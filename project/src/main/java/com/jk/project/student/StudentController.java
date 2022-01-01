ab081e807866dc457bc676fe73d35ff29a4c1b2c
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2022-01-01 20:14:23 +0800

    Created the first rest endpoint to get all students

---
+++ project/src/main/java/com/jk/project/student/StudentController.java
@@ -0,0 +1,34 @@
+package com.jk.project.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return List.of(
+                new Student(
+                        UUID.randomUUID(),
+                        "Jeremy",
+                        "Ponto",
+                        "jeremyponto@gmail.com",
+                        Student.Gender.MALE
+                ),
+                new Student(
+                        UUID.randomUUID(),
+                        "Kevin",
+                        "Herman",
+                        "kevinherman@gmail.com",
+                        Student.Gender.MALE
+                )
+        );
+    }
+
+}
