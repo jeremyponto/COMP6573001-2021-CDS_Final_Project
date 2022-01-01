67da8443e8e4f7a64537514b230ea6be9ea9455b
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2022-01-02 03:12:40 +0800

    Added a placeholder text and called a function from client.js to log all students in console

---
+++ project/src/js/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { getAllStudents } from './client';
+
+function App() {
+  getAllStudents().then(res => res.json().then(students => {
+    console.log(students);
+  }));
+  return <h1>Hello World Spring Boot & React</h1>
+}
+
+export default App;
