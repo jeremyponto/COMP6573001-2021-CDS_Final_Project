7b0f994d8e981e3c6dc10addc2acc934da1a6bc4
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2022-01-03 07:50:11 +0800

    Created a custom API Request Exception

---
+++ project/src/main/java/com/jk/project/exception/ApiRequestException.java
@@ -0,0 +1,13 @@
+package com.jk.project.exception;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
