f52d8159fe064fd00d771724d5300fdec23bfb5f
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2022-01-03 02:14:55 +0800

    Added a Footer to show the number of students in an Avatar and to be put an Add New Student + Button

---
+++ project/src/js/src/Footer.js
@@ -0,0 +1,19 @@
+import { Button, Avatar } from "antd";
+import React from "react";
+import Container from "./Container";
+import './Footer.css';
+
+const Footer = (props) => (
+    <div className='footer'>
+        <Container>
+            {props.numberOfStudents ?
+                <Avatar
+                    style={{backgroundColor: '#f56a00', marginRight: '5px'}}
+                    size='large'>{props.numberOfStudents}</Avatar> : null
+            }
+            <Button onClick={() => props.handleAddStudentClickEvent()} type='primary'>Add New Student +</Button>
+        </Container>
+    </div>
+);
+
+export default Footer;
