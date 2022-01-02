191a638e3af32c619841b1f0f518b43484600551
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2022-01-03 05:29:38 +0800

    Used addNewStudent function to add a new student on submit and displayed the added student

--- project/src/js/src/forms/AddStudentForm.js
+++ project/src/js/src/forms/AddStudentForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Formik } from "formik";
 import { Input, Button, Tag } from "antd";
+import { addNewStudent } from "../client";
 
 const inputBottomMargin = {marginBottom: '10px'};
 const tagStyle = {backgroundColor: '#f50', color: 'white', borderRadius: '2px', ...inputBottomMargin};
@@ -35,12 +36,12 @@ class AddStudentForm extends Component {
                 
                 return errors;
             }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    
+            onSubmit={(student, { setSubmitting }) => {
+                addNewStudent(student).then(() => {
+                    alert(JSON.stringify(student));
+
                     setSubmitting(false);
-                }, 400);
+                })
             }}
             >
             {({
