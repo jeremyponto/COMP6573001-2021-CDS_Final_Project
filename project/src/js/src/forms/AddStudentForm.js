bfb00f9aa58095ae49a90676836a5146de608643
jeremyponto <56902787+jeremyponto@users.noreply.github.com>
2022-01-03 03:34:11 +0800

    Customized the form to match with the student information

--- project/src/js/src/forms/AddStudentForm.js
+++ project/src/js/src/forms/AddStudentForm.js
@@ -1,65 +1,86 @@
 import React, { Component } from "react";
 import { Formik } from "formik";
+import { Input, Button } from "antd";
+
+const inputBottomMargin = {marginBottom: '10px'};
 
 class AddStudentForm extends Component {
     render() {
         return(
-            <div>
-                <h1>Anywhere in your app!</h1>
-                <Formik
-                initialValues={{ email: '', password: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                    errors.email = 'Required';
-                    } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                    errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                    }, 400);
-                }}
-                >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    /* and other goodies */
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                    <input
-                        type="email"
+            <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+                const errors = {};
+                if (!values.email) {
+                errors.email = 'Required';
+                } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+                }, 400);
+            }}
+            >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                /* and other goodies */
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <Input
+                        style={inputBottomMargin}
+                        name="firstName"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.firstName}
+                        placeholder='First name. E.g John'
+                    />
+                    {errors.firstName && touched.firstName && errors.firstName}
+                    <Input
+                        style={inputBottomMargin}
+                        name="lastName"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.lastName}
+                        placeholder='Last name. E.g Doe'
+                    />
+                    {errors.lastName && touched.lastName && errors.lastName}
+                    <Input
+                        style={inputBottomMargin}
                         name="email"
+                        type='email'
                         onChange={handleChange}
                         onBlur={handleBlur}
                         value={values.email}
+                        placeholder='Email. E.g example.gmail.com'
                     />
                     {errors.email && touched.email && errors.email}
-                    <input
-                        type="password"
-                        name="password"
+                    <Input
+                        style={inputBottomMargin}
+                        name="gender"
                         onChange={handleChange}
                         onBlur={handleBlur}
-                        value={values.password}
+                        value={values.gender}
+                        placeholder='Gender. E.g Male or Female'
                     />
-                    {errors.password && touched.password && errors.password}
-                    <button type="submit" disabled={isSubmitting}>
+                    {errors.gender && touched.gender && errors.gender}
+                    <Button type="submit" disabled={isSubmitting}>
                         Submit
-                    </button>
-                    </form>
-                )}
-                </Formik>
-            </div>
+                    </Button>
+                </form>
+            )}
+            </Formik>
         )
     }
 }
